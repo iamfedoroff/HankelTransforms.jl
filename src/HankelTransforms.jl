dec1f4da54133d0292b60e8844af12f5b46e0d2a
iamfedoroff <iamfedoroff@gmail.com>
2021-05-26 14:03:11 +0300

    In import statements explicitly mention imported functions.

--- src/HankelTransforms.jl
+++ src/HankelTransforms.jl
@@ -1,16 +1,30 @@
 module HankelTransforms
 
 import Base: *, \
-import JLD2
-import LinearAlgebra
+import CUDA: CuArray, CuDeviceArray, @cuda, launch_configuration,
+             threadIdx, blockIdx, blockDim, gridDim
+import JLD2: @save, @load
+import Roots: fzero
+import SpecialFunctions: besselj
+
+export plan, htcoord, htfreq, dht!, dht, idht!, idht
 
 
-# CPU / GPU specific functions -------------------------------------------------
-import CUDA
+# Adopted from FunctionZeros.jl
+# Asymptotic formula for the n'th zero of Bessel J function of order nu
+besselj_zero_asymptotic(nu, n) = pi * (n - 1 + nu / 2 + 3//4)
+
+
+# Adopted from FunctionZeros.jl
+# Use the asymptotic values as starting values. These find the correct zeros
+# even for n = 1,2,... Order 0 is 6 times slower and 50-100 times less accurate
+# than higher orders, with other parameters constant.
+besselj_zero(nu, n; order=2) =
+    fzero((x) -> besselj(nu, x), besselj_zero_asymptotic(nu, n); order=order)
 
 
 function isongpu(T)
-    if T <: CUDA.CuArray
+    if T <: CuArray
         IOG = true
     else
         IOG = false
@@ -27,8 +41,8 @@ macro krun(ex...)
 
     @gensym kernel config threads blocks
     code = quote
-        local $kernel = CUDA.@cuda launch=false $call
-        local $config = CUDA.launch_configuration($kernel.fun)
+        local $kernel = @cuda launch=false $call
+        local $config = launch_configuration($kernel.fun)
         local $threads = min($N, $config.threads)
         local $blocks = cld($N, $threads)
         $kernel($(args...); threads=$threads, blocks=$blocks)
@@ -37,42 +51,6 @@ macro krun(ex...)
     return esc(code)
 end
 
-
-# Bessel functions -------------------------------------------------------------
-# GSL is the Julia wrapper for the GNU Scientific Library (GSL) and does not
-# work under Windows.
-# import GSL
-# bessel(p, x) = GSL.sf_bessel_Jn(p, x)
-# bessel_zero(p, n) = GSL.sf_bessel_zero_Jnu(p, n)
-
-import SpecialFunctions
-bessel(p, x) = SpecialFunctions.besselj(p, x)
-
-# import FunctionZeros
-# bessel_zero(p, n) = FunctionZeros.besselj_zero(p, n)
-
-# Since FunctionZeros package is too slow in updating of versions for
-# SpecialFunctions, I copy all necessary functions here:
-
-import Roots
-
-# Asymptotic formula for the n'th zero of Bessel J function of order nu
-besselj_zero_asymptotic(nu, n) = pi * (n - 1 + nu / 2 + 3//4)
-
-# Use the asymptotic values as starting values. These find the correct zeros
-# even for n = 1,2,... Order 0 is 6 times slower and 50-100 times less accurate
-# than higher orders, with other parameters constant.
-besselj_zero(nu, n; order=2) =
-    Roots.fzero(
-        (x) -> SpecialFunctions.besselj(nu, x),
-        besselj_zero_asymptotic(nu, n); order=order
-    )
-
-bessel_zero(p, n) = besselj_zero(p, n)
-# ------------------------------------------------------------------------------
-
-
-export plan, htcoord, htfreq, dht!, dht, idht!, idht
 
 const htAbstractArray{T} = Union{AbstractArray{T}, AbstractArray{Complex{T}}}
 
@@ -118,19 +96,19 @@ function plan(
     J = zeros(T, N)
     TT = zeros(T, (N, N))
 
-    @. a = bessel_zero(p, 1:N)
-    aNp1::T = bessel_zero(p, N + 1)
+    @. a = besselj_zero(p, 1:N)
+    aNp1::T = besselj_zero(p, N + 1)
 
     V::T = aNp1 / (2 * pi * R)
-    @. J = abs(bessel(p + 1, a)) / R
+    @. J = abs(besselj(p + 1, a)) / R
 
     S::T = 2 * pi * R * V
 
     for j=1:N
     for i=1:N
-        TT[i, j] = 2 * bessel(p, a[i] * a[j] / S) /
-                   abs(bessel(p + 1, a[i])) /
-                   abs(bessel(p + 1, a[j])) / S
+        TT[i, j] = 2 * besselj(p, a[i] * a[j] / S) /
+                   abs(besselj(p + 1, a[i])) /
+                   abs(besselj(p + 1, a[j])) / S
     end
     end
 
@@ -138,9 +116,9 @@ function plan(
 
     IOG = isongpu(UF)
     if IOG
-        J = CUDA.CuArray(J)
-        TT = CUDA.CuArray(TT)
-        ftmp = CUDA.CuArray(ftmp)
+        J = CuArray(J)
+        TT = CuArray(TT)
+        ftmp = CuArray(ftmp)
     end
 
     CI = typeof(region)
@@ -149,7 +127,7 @@ function plan(
     plan = Plan{IOG, CI, T, UJ, UT, UF}(N, region, R, V, J, TT, ftmp)
 
     if save
-        JLD2.@save fname plan
+        @save fname plan
     end
 
     return plan
@@ -158,7 +136,7 @@ end
 
 function plan(fname::String)
     plan = nothing
-    JLD2.@load fname plan
+    @load fname plan
     return plan
 end
 
@@ -168,8 +146,8 @@ Compute the spatial coordinates for Hankel transform.
 """
 function htcoord(R::T, N::I, p::I=0) where {T<:AbstractFloat, I<:Int}
     a = zeros(T, N)
-    @. a = bessel_zero(p, 1:N)
-    aNp1::T = bessel_zero(p, N + 1)
+    @. a = besselj_zero(p, 1:N)
+    aNp1::T = besselj_zero(p, N + 1)
     V::T = aNp1 / (2 * pi * R)
     @. a = a / (2 * pi * V)   # resuse the same array to avoid allocations
     return a
@@ -181,7 +159,7 @@ Compute the spatial frequencies (ordinary, not angular) for Hankel transform.
 """
 function htfreq(R::T, N::I, p::I=0) where {T<:AbstractFloat, I<:Int}
     a = zeros(T, N)
-    @. a = bessel_zero(p, 1:N)
+    @. a = besselj_zero(p, 1:N)
     @. a = a / (2 * pi * R)   # resuse the same array to avoid allocations
     return a
 end
@@ -342,9 +320,9 @@ function idht!(
 end
 
 
-function kernel1(f::CUDA.CuDeviceArray, J, RV, region)
-    id = (CUDA.blockIdx().x - 1) * CUDA.blockDim().x + CUDA.threadIdx().x
-    stride = CUDA.blockDim().x * CUDA.gridDim().x
+function kernel1(f::CuDeviceArray, J, RV, region)
+    id = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+    stride = blockDim().x * gridDim().x
     # axis = 1
     N = length(region)
     for k=id:stride:N
@@ -355,9 +333,9 @@ function kernel1(f::CUDA.CuDeviceArray, J, RV, region)
 end
 
 
-function kernel2(f::CUDA.CuDeviceArray{T, 1}, ftmp, TT, region) where T
-    id = (CUDA.blockIdx().x - 1) * CUDA.blockDim().x + CUDA.threadIdx().x
-    stride = CUDA.blockDim().x * CUDA.gridDim().x
+function kernel2(f::CuDeviceArray{T, 1}, ftmp, TT, region) where T
+    id = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+    stride = blockDim().x * gridDim().x
     # axis = 1
     N = length(region)
     Naxis = size(region)[1]   # Naxis = size(region)[axis]
@@ -372,9 +350,9 @@ function kernel2(f::CUDA.CuDeviceArray{T, 1}, ftmp, TT, region) where T
 end
 
 
-function kernel2(f::CUDA.CuDeviceArray{T, 2}, ftmp, TT, region) where T
-    id = (CUDA.blockIdx().x - 1) * CUDA.blockDim().x + CUDA.threadIdx().x
-    stride = CUDA.blockDim().x * CUDA.gridDim().x
+function kernel2(f::CuDeviceArray{T, 2}, ftmp, TT, region) where T
+    id = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+    stride = blockDim().x * gridDim().x
     # axis = 1
     N = length(region)
     Naxis = size(region)[1]   # Naxis = size(region)[axis]
@@ -390,9 +368,9 @@ function kernel2(f::CUDA.CuDeviceArray{T, 2}, ftmp, TT, region) where T
 end
 
 
-function kernel3(f::CUDA.CuDeviceArray, ftmp, J, RV, region)
-    id = (CUDA.blockIdx().x - 1) * CUDA.blockDim().x + CUDA.threadIdx().x
-    stride = CUDA.blockDim().x * CUDA.gridDim().x
+function kernel3(f::CuDeviceArray, ftmp, J, RV, region)
+    id = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+    stride = blockDim().x * gridDim().x
     # axis = 1
     N = length(region)
     for k=id:stride:N
